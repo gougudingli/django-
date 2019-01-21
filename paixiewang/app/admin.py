59c392e1147e2115498014e01201cda99bc7fcfc
gougudingli <343797109@qq.com>
2019-01-21 20:33:53 +0800

    del

--- paixiewang/app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from app.models import User
-
-admin.site.register(User)
--- paixiewang/static/js/liuchang.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $('img').each(function () {
-        srcText = $(this).attr('src')
-        console.log(srcText)
-        $(this).attr('src',"{% static '"+srcText+"'%}")
-        srcText1 = $(this).attr('src')
-    })
-    // console.log($('body').html())
-})
